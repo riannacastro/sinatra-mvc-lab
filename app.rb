9e5f63d5de76f41e6443155cafc13f80413f5d34
Rianna Castro <riannaxcastro@gmail.com>
2021-05-25 15:07:54 -1000

    commit tree b0617bb880e5a6edcca4a3ddee8d346f4d496d2d

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        new_pig = PigLatinizer.new
+        new_pig.piglatinize(params[:user_phrase])
+    end
+    
+
 end
